57951096fe1ac6bc0c7acb23b7dce4b5178a3c2c
wario123 <ka295@cornell.edu>
2018-10-30 17:28:21 -0400

    Adding basic xcode project

---
+++ ProductFinder/ProductFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductFinder
+//
+//  Created by Karim Arem on 10/30/18.
+//  Copyright © 2018 Karim Arem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
